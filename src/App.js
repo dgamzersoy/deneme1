5a53150eaa71b9dd6e5dd3f74c4d071611f95ced
Dilek Gamze <dgamzersoy@outlook.com>
2021-06-04 13:31:20 +0300

    fpoırpowe

--- src/App.js
+++ src/App.js
@@ -5,22 +5,40 @@ function App() {
   return (
     <div>
       <div class="header">
-         <h1>header</h1>
+        <h1>header</h1>
+      </div>
+      <div class="path">
+      <h1>path</h1>
+      </div>
+    
+      <div class="docktop">
+      <h1>docktop</h1>
       </div>
 
-      <div class="sidebar">
-       <h1>sidebar</h1>
+      <div class="kutu1">
+        <div class="dockleft">
+        <h1>dockleft</h1>
+        </div>
+        <div class="kutu2">
+        <div class="error">
+        <h1>error</h1>
+        </div>
+        <div class="content">
+        <h1>content</h1>
+        </div>
+        </div>
+        <div class="dockright">
+        <h1>dockright</h1>
+        </div>
       </div>
 
-      <div class="content">
-      <h1>content</h1>
+      <div class="dockbottom">
+      <h1>dockbottom</h1>
       </div>
-      
       <div class="footer">
-       <h1>footer</h1>
+      <h1>footer</h1>
       </div>
-      
-    </div>
+  </div>
   );
 }
 
